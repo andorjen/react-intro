22547b9e8fd30076e52dd06398bc7ab6831f255b
Jenny Liu <jennyliuye@gmail.com>
2021-10-11 12:17:56 -0700

    part3

---
+++ PART-3/App.js
@@ -0,0 +1,8 @@
+function App() {
+    return (
+        <div>
+            <Person name='Colin' age="15" hobbies={["rock climbing", "yoga", "weight lifting"]} />
+            <Person name='Jenny' age="21" hobbies={["napping", "eating", "firing"]} />
+        </div>
+    )
+}
